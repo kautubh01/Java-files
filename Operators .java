a13be03a4dba9895b11a15c3c850ce6b1804b9fb
Kaustubh Pandey <146510603+kautubh01@users.noreply.github.com>
2024-08-18 22:37:40 +0530

    Create Operators .java

---
+++ Operators .java
@@ -0,0 +1,26 @@
+class Operator {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 5;
+        int result = num1 + num2;
+        int result1 = num1 - num2;
+        int result2 = num1 * num2;
+        int result3 = num1/num2;
+        int result4 = num1%num2;
+        num1 += 8;
+        num1++;  //post incremeant
+        //++num1  pre increment
+        System.out.println(num1);
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+
+
+
+
+    }
+    
+}
