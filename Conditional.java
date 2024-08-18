9713e4b391536b1306d9b7e2e3f6e5d6cdf1b66a
Kaustubh Pandey <146510603+kautubh01@users.noreply.github.com>
2024-08-18 22:44:46 +0530

    Create Conditional.java

---
+++ Conditional.java
@@ -0,0 +1,41 @@
+ class Conditional {
+    public static void main(String[] args) {
+        int x = 189;
+        if(x>10 && x<=20){
+            System.out.println("helo");
+        }
+        else{
+            System.out.println("byeee");
+        }
+       int a = 76;
+       int b = 86;
+        if (a>b) {
+            System.out.println(a);
+            System.out.println("Thankyou");
+            
+        }
+        else{
+            System.out.println(b);
+        }
+        //if elseif
+        int c = 98;
+        int d = 199;
+        int e = 100;
+        if (c>d && c>e) {
+            System.out.println("greatest is c");
+            
+        }
+        else if(d>c && d>e)
+        {
+            System.out.println("greatest is d");
+
+        }
+        else{
+            System.out.println("greatest is e");
+        }
+
+    }
+
+    
+}
+ 
