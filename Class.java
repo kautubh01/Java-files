9ee5dfc3b5977834f54fdaac5d826cf7cfec2092
Kaustubh Pandey <146510603+kautubh01@users.noreply.github.com>
2024-08-19 10:56:11 +0530

    added code

--- Class.java
+++ Class.java
@@ -1,1 +1,21 @@
 
+class employee{
+    String name;
+    int age;
+    int salary;
+    public void printdetails(){
+        System.out.println("the name of employee:"+ name);
+        System.out.println("the age of employee:"+age);
+        System.out.println("the salary of employee:"+salary);
+    }
+}
+public class Class {
+    public static void main(String[] args) {
+        employee john = new employee();
+        john.name="John Micheil";
+        john.age=23;
+        john.salary=23000;
+        john.printdetails();
+    }
+    
+}
