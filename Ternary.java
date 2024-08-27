94d8d51779bf024f01bc09d6e7ab330e2108b538
Kaustubh Pandey <146510603+kautubh01@users.noreply.github.com>
2024-08-27 22:52:13 +0530

    Create Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,18 @@
+ class Ternary {
+    public static void main(String[] args) {
+        int n = 4;
+        int result = 0;
+        result = n%2==0 ? 10:20;
+        System.out.println(result);
+
+        int a = 6;
+        int result1 =0;
+        result1 = a%2==0 ? 20:99;
+        System.out.println(result1);
+        
+
+
+
+        
+    }
+}
